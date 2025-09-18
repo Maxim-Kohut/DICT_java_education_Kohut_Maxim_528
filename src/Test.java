b2072c3b870b8555b0261d8ec09e6393e1b63289
Maxim-Kohut <m.i.kohut@student.khai.edu>
2025-09-18 10:46:16 +0300

    Test.java added to feature branch
    add a comment to Test.java on feature branch
    
    added one more print to Test.java on the feature branch

--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,11 @@
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Java");
+<<<<<<< HEAD
+        System.out.println("Java education");
+=======
+        System.out.println("education");
+        //comment
+        System.out.println("DICT");
+>>>>>>> 213eab7 (added one more print to Test.java on the feature branch)
     }
 }
