75d521e394c0d7ccf6c20d2044bd2d7bb50d4945
Maxim-Kohut <m.i.kohut@student.khai.edu>
2025-09-30 18:06:22 +0300

    ChatBot Andrew st.1

---
+++ src/ChatBot/chatBot.java
@@ -0,0 +1,7 @@
+public class chatBot {
+    public static void main (String[] args)
+    {
+        System.out.println("Hello! I'm ChatBot. My name is Andrew.");
+        System.out.println("I was created in 2025");
+    }
+}
