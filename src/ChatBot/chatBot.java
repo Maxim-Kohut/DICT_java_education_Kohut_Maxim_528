b7473f7e7a7aa865b1fc55b49c6a19efb3c087cc
Maxim-Kohut <m.i.kohut@student.khai.edu>
2025-09-30 18:13:11 +0300

    ChatBot Andrew st.2

--- src/ChatBot/chatBot.java
+++ src/ChatBot/chatBot.java
@@ -1,7 +1,13 @@
+import java.util.Scanner;
+
 public class chatBot {
     public static void main (String[] args)
     {
         System.out.println("Hello! I'm ChatBot. My name is Andrew.");
         System.out.println("I was created in 2025");
+        System.out.println("Pls remind your name");
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+        System.out.println("Great name " + name);
     }
 }
