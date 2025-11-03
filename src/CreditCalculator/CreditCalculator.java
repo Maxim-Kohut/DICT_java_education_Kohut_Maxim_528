2949f6b644548688dc903b27f7219c5993a48dee
Maxim-Kohut <m.i.kohut@student.khai.edu>
2025-11-03 08:08:21 +0200

    Calc St.1

---
+++ src/CreditCalculator/CreditCalculator.java
@@ -0,0 +1,8 @@
+public class CreditCalculator {
+    public static void main(String[] args) {
+        Math_and_logic logic = new Math_and_logic();
+
+        // St.1: проверим просто вывод
+        logic.demoOutput();
+    }
+}
---
+++ src/CreditCalculator/Math_and_logic.java
@@ -0,0 +1,12 @@
+public class Math_and_logic {
+
+    // St.1
+    public void demoOutput() {
+        // фиктивные платежи
+        System.out.println("Loan principal: 1000");
+        System.out.println("Month 1: repaid 250");
+        System.out.println("Month 2: repaid 250");
+        System.out.println("Month 3: repaid 500");
+        System.out.println("The loan has been repaid!");
+    }
+}
